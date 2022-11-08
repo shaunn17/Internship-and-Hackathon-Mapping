fddd473809325addb5e497c1cf96aaa5bdf903bf
Shaun Figueiro <72642251+shaunn17@users.noreply.github.com>
2022-11-08 14:44:05 +0530

    Update recommendation.js

--- routes/api/recommendation.js
+++ routes/api/recommendation.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
-const Recommendation = require('../../models/Recommendation');
+const Recommendation = require('../../models/Recommendation'); 
 
 // @route    GET api/recommendation
 // @desc     Get all recommendation
